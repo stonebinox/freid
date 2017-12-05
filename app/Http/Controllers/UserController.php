03bbd1376988a9e0f1f0fd0a299a2e0a89fc0c93
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-12-05 22:08:41 +0100

    User Onboarding

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+
+    public function profileType(Request $request)
+    {
+        $method = $request->isMethod('post');
+
+        switch($method){
+            case true:
+                if ($request->profile_type == '1'){
+                    return redirect()->route('expert_info');
+                } else {
+                    return redirect()->route('welcome');
+                }
+            default:
+                return view('users.profile_type');
+        }
+    }
+
+    public function expertInfo(Request $request)
+    {
+        $method = $request->isMethod('post');
+
+        switch($method){
+            case true:
+                $request->validate([
+                    'skills'        => 'required',
+                    'headline'      => 'required',
+                    'description'   => 'required',
+                ]);
+
+
+                $user = User::find(Auth::user()->id);
+
+                $user->skills       = $request->skills;
+                $user->headline     = $request->headline;
+                $user->description  = $request->description;
+                $user->save();
+
+                return redirect()->route('welcome');
+            default:
+                return view('users.expert_info');
+        }
+    }
+
 }
