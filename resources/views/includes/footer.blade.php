53d8630863c306995120c6a4460132b93cc8ed48
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-12-08 00:33:52 +0100

    All Complete

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,30 +2,22 @@
         <div class="container">
             <p><span>©2017 Freid</span></p>
 
-            <ul id="menu-footer-menu">
+            <!--<ul id="menu-footer-menu">
                 <li>
                     <a href="#">About Us</a>
                 </li>
-                <!-- /li -->
                 <li>
                     <a href="#">Privacy</a>
                 </li>
-                <!-- /li -->
                 <li>
                     <a href="#">Terms & Conditions</a>
                 </li>
-                <!-- /li -->
                 <li>
                     <a href="#">Contact Us</a>
                 </li>
-                <!-- /li -->
-            </ul>
-            <!-- /ul -->
-
+            </ul>-->
         </div>
-        <!-- /container -->
     </footer>
-    <!-- /footer -->
 
     <a id="scrollup">Scroll</a>
 
