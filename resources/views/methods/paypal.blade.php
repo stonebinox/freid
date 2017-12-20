18a7cb4ee8ea2615d56f591c21f48126ef5bc687
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-12-20 12:48:18 +0100

    Changed Payment Header

--- resources/views/methods/paypal.blade.php
+++ resources/views/methods/paypal.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
                     <div class="post-heading text-center">
-                        <h1>Edit Your Profile:</h1>
+                        <h1>Add PayPal Account:</h1>
                         <hr style="background: white;">
                     </div>
                 </div>
